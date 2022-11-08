7b65edf1aa35d838f8378a301a7a8599af7640a2
Ingkawat 63070189 <73688553+Ingkawat@users.noreply.github.com>
2022-11-08 23:06:22 +0700

    Delete backend directory

--- backend/mongoose.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/mobile', {
-    useNewUrlParser: true,
-  }).then(()=>{
-      
-  }).catch((eer)=>{
-      console.log('someting wrong', eer)
-  })
